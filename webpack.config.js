92f40c948877f66d50981f624b924c5f43a7eaac
Jake Runzer <jakerunzer@gmail.com>
2019-09-27 11:20:22 +0100

    use identity obj proxy for both styles and files

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
       },
       styleLoaders,
       {
-        test: /\.(png|jpe?g|svg|ico)$/,
+        test: /\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/,
         loader: "file-loader",
       },
     ],
